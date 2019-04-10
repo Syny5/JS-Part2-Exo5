24bd526857ec9dd6ce032b01831a33794f2cf7a6
syn <vicdemilly@gmail.com>
2019-04-10 14:58:22 +0200

    Changer les images en hover avec une fonction JS

---
+++ main.js
@@ -0,0 +1,7 @@
+function changeImage() {
+  document.getElementById('image1').src='image1_2.jpg';
+  document.getElementById('image2').src='image2_2.jpg';
+  document.getElementById('image3').src='image3_2.jpg';
+  document.getElementById('image4').src='image4_2.jpg';
+  document.getElementById('image5').src='image5_2.jpg';
+}
